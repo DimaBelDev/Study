6c4f3b338ec851a2ef6ecdc6e7ad67c7aa728a9a
Dima Salita <salitadima1@gmail.com>
2022-04-16 16:32:50 +0300

    peek

--- src/net/study/stream/lesson9_peek.java
+++ src/net/study/stream/lesson9_peek.java
@@ -1,7 +1,12 @@
 package net.study.stream;
 
+import java.util.stream.Stream;
+
 public class lesson9_peek {
     public static void main(String[] args) {
+        Stream<Integer> stream1 = Stream.of(1,2,3,4,5,6,1,2,3);
 
+        System.out.println(stream1.distinct().peek(System.out::println).count());
+        // peek не терминальный, используется для просмотра что происходит со стримом
     }
 }
