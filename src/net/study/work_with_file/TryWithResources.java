2562c5164aff41e86317df1c7eff7ab09c0525ab
Dima Salita <salitadima1@gmail.com>
2022-04-22 01:20:31 +0300

    TryWithResources

---
+++ src/net/study/work_with_file/TryWithResources.java
@@ -0,0 +1,18 @@
+package net.study.work_with_file;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TryWithResources {
+    public static void main(String[] args) {
+        String s = "Hello world!";
+
+
+        try (FileWriter fileWriter = new FileWriter("C:\\Users\\Anonymous\\Desktop\\test1.txt", true);){
+            fileWriter.write(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
