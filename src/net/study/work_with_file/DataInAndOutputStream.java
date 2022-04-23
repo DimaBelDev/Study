ca2b2f9a66916f2aefe34c7c9d082b6e4ada1687
Dima Salita <salitadima1@gmail.com>
2022-04-23 21:13:33 +0300

    DataInAndOutputStream

---
+++ src/net/study/work_with_file/DataInAndOutputStream.java
@@ -0,0 +1,29 @@
+package net.study.work_with_file;
+
+import java.io.*;
+
+public class DataInAndOutputStream {
+    public static void main(String[] args) {
+        // формат bin (Файл BIN хранит данные в двоичном(бинарном) формате)
+        // Используется для примитивных типов данных
+
+        try(DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream("java_simple.bin"));
+                DataInputStream dataInputStream = new DataInputStream(new FileInputStream("java_simple.bin"))){
+              dataOutputStream.writeByte(123);
+              dataOutputStream.writeBoolean(true);
+              dataOutputStream.writeInt(1_000);
+              dataOutputStream.writeChar('n');
+              dataOutputStream.writeLong(1_000_000L);
+            System.out.println(dataInputStream.readByte());
+            System.out.println(dataInputStream.readBoolean());
+            System.out.println(dataInputStream.readInt());
+            System.out.println(dataInputStream.readChar());
+            System.out.println(dataInputStream.readLong());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
