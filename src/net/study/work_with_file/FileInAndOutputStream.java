77fdb2a572e4640f1a429b6ac9e834345edd21eb
Dima Salita <salitadima1@gmail.com>
2022-04-23 17:27:24 +0300

    FileInAndOutputStream

---
+++ src/net/study/work_with_file/FileInAndOutputStream.java
@@ -0,0 +1,23 @@
+package net.study.work_with_file;
+
+import java.io.*;
+
+public class FileInAndOutputStream {
+    public static void main(String[] args) {
+        // используем с бинарными данными
+        //BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream("C:\\Users\\Anonymous\\Desktop\\java_pic.png"));
+        // принцип работы буфера одинаковый с FileWriter или FileReader
+        try(FileInputStream fileInputStream = new FileInputStream("C:\\Users\\Anonymous\\Desktop\\java_pic.png");
+            FileOutputStream fileOutputStream = new FileOutputStream("java-pic.jpeg")){
+            int temp;
+            while ((temp=fileInputStream.read()) != -1){
+                fileOutputStream.write(temp);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
